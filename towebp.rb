b2f5e9a87c1c495b428cbec6c02b2c2bb6ec2528
kugyu10 <kugyu10@gmail.com>
2022-08-18 23:04:54 +0900

    fix for automator

--- towebp.rb
+++ towebp.rb
@@ -1,36 +1,38 @@
+# encoding: utf-8
 # 引数に gif/png/jpg 画像ファイルを指定すると、同名のwebp画像に変換する
-#p "towebp.rb start."
-inFileName = ARGV[0]
-#p 'inFileName: ' + inFileName
+ARGV.each do |f|
 
+  p "towebp.rb start."
+  inFileName = f
+  p 'inFileName: ' + inFileName
+  
+  if( !inFileName || !File.exist?(inFileName) ) then
+    p '引数が不正です'
+    return 1
+  end
+  
+  inFileName = f
+  extention = File.extname(inFileName)
+  basename = File.basename(inFileName, extention )
+  outFileName = File.dirname(f) +"/"+ basename + '.webp'
 
-#p !inFileName
-#p !File.exist?(inFileName)
+  #同名回避
+  fileNum = 0
+  while( File.exist?(outFileName) ) do
+    fileNum += 1
+    outFileName = basename + '_' + fileNum.to_s + '.webp'
+  end
 
-
-if( !inFileName || !File.exist?(inFileName) ) then
-  p '引数が不正です'
-  return 1
+  p 'outFileName: ' + outFileName
+  
+  if ('.gif' == extention.downcase) then
+    p 'gif2webp:'
+    `/usr/local/bin/gif2webp -q 100 -metadata icc -o #{outFileName} #{inFileName}`
+  elsif ( '.png' == extention.downcase || '.jpeg' == extention.downcase || '.jpg' == extention.downcase ) then
+    p 'cwebp:'
+    p `/usr/local/bin/cwebp -q 80 -metadata icc -o #{outFileName} #{inFileName}`
+  end
+  
+  p "toweb.rb end."
 end
-
-inFileName = ARGV[0]
-extention = File.extname(inFileName)
-#p extention
-basename = File.basename(inFileName, extention )
-
-outFileName = basename + '.webp'
-#同名回避
-fileNum = 0
-while( File.exist?(outFileName) ) do
-  fileNum += 1
-  outFileName = basename + '_' + fileNum.to_s + '.webp'
-end
-
-if ('.gif' == extention.downcase) then
-  exec "gif2webp -q 100 -metadata icc -o " + outFileName + " " + inFileName
-  p 'gif2webp'
-elsif ( '.png' == extention.downcase || '.jpeg' == extention.downcase || '.jpg' == extention.downcase ) then
-  exec "cwebp -q 80 -metadata icc -o " + outFileName + " " +inFileName
-  p 'cwebp'
-end
-
+  
